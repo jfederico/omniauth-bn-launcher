3967e9b179cb14ea34d89e5357cc272bbc8fafd4
bruckwubete <bruckwendwessenwubet@cmail.carleton.ca>
2018-07-03 15:31:12 -0400

    raw info path edit

--- lib/omniauth/strategies/bn_launcher.rb
+++ lib/omniauth/strategies/bn_launcher.rb
@@ -28,7 +28,7 @@ module OmniAuth
       end
 
       def raw_info
-        @raw_info ||= access_token.get('/me').parsed
+        @raw_info ||= access_token.get('/user').parsed
       end
     end
   end
