c7c90ecf19f957778fe27267806d7be859388d65
bruckwubete <bruckwendwessenwubet@cmail.carleton.ca>
2018-07-03 14:57:33 -0400

    switching to oauth2

--- lib/omniauth/strategies/bn_launcher.rb
+++ lib/omniauth/strategies/bn_launcher.rb
@@ -1,52 +1,34 @@
 require 'omniauth'
 require 'digest'
+require 'omniauth-oauth2'
 module OmniAuth
   module Strategies
-    class BnLauncher
-      include OmniAuth::Strategy
+    class BnLauncher < OmniAuth::Strategies::OAuth2
 
       option :name, 'bn_launcher'
 
-      # # receive parameters from the strategy declaration and save them
-      def initialize(app, secret, auth_redirect, options = {})
-        @secret = secret
-        @auth_redirect = auth_redirect
-        @options = options
-        super(app, options)
+      # These are called after authentication has succeeded. If
+      # possible, you should try to set the UID without making
+      # additional calls (if the user id is returned with the token
+      # or as a URI parameter). This may not be possible with all
+      # providers.
+      uid{ raw_info['id'] }
+
+      info do
+        {
+            :name => raw_info['name'],
+            :email => raw_info['email']
+        }
       end
 
-      # redirect to the auth website
-      def request_phase
-        r = Rack::Response.new
-        r.redirect request.params["redirect_uri"] || @auth_redirect
-        r.finish
+      extra do
+        {
+            'raw_info' => raw_info
+        }
       end
 
-      def callback_phase
-        uid, username, email, provider, checksum = request.params["uid"], request.params["username"], request.params["email"], request.params["provider"], request.params["checksum"]
-        sha1 = Digest::SHA1.hexdigest("#{@secret}#{uid}#{username}#{email}#{provider}")
-
-        # check if the request comes from valid source or not
-        if sha1 == checksum
-          @uid, @username, @email, @provider = uid, username, email, provider
-          # OmniAuth takes care of the rest
-          super
-        else
-          # OmniAuth takes care of the rest
-          fail!(:invalid_credentials)
-        end
-      end
-
-      # normalize user's data according to http://github.com/intridea/omniauth/wiki/Auth-Hash-Schema
-      def auth_hash
-        OmniAuth::Utils.deep_merge(super(), {
-            'uid' => @uid,
-            'user_info' => {
-                'username' => @username,
-                'email'    => @email,
-                'provider' => @provider
-            }
-        })
+      def raw_info
+        @raw_info ||= access_token.get('/me').parsed
       end
     end
   end
--- omniauth-bn-launcher.gemspec
+++ omniauth-bn-launcher.gemspec
@@ -34,6 +34,8 @@ Gem::Specification.new do |spec|
   spec.add_development_dependency "rake", "~> 10.0"
   spec.add_development_dependency "rspec", "~> 3.0"
   spec.add_development_dependency 'rack-test', '~> 0.6', '>= 0.6.3'
+  spec.add_development_dependency 'omniauth-oauth2', '1.5.0'
   spec.add_runtime_dependency 'omniauth', '~> 1.3', '>= 1.3.2'
+  spec.add_runtime_dependency 'omniauth-oauth2', '1.5.0'
   spec.test_files    = Dir['spec/**/*.rb']
 end
