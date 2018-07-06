255f3386c8ccfb83d66acc25282914a89c3dd422
bruckwubete <bruckwendwessenwubet@cmail.carleton.ca>
2018-07-06 10:29:12 -0400

    query string

--- lib/omniauth/strategies/bn_launcher.rb
+++ lib/omniauth/strategies/bn_launcher.rb
@@ -15,7 +15,8 @@ module OmniAuth
 
       def callback_phase
         puts "BN LAUNCHER DEBUG:  ", !options.provider_ignores_state, request.params["state"].to_s.empty?, request.params["state"], session["omniauth.state"]
-        redirect_to "http://ice.gl.greenlight.com:4000/auth/bn_launcher/callback?#{request.query_parameters.to_param}" and return if request.host == "demo.gl.greenlight.com"
+        puts request.query_string
+        redirect_to "http://ice.gl.greenlight.com:4000/auth/bn_launcher/callback?#{request.query_string}" and return if request.host == "demo.gl.greenlight.com"
         super
       end
 
